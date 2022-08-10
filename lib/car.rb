c20949b04e5ce186832034b6aecab56671fb6333
Karter Livingston <kdlmuzixnerd14@gmail.com>
2022-08-10 15:02:13 -0700

    commit tree 758f7d3aa7334b7c197e8635221ad45b62204b73

--- lib/car.rb
+++ lib/car.rb
@@ -1,2 +1,12 @@
 # This loads the code from the vehicle.rb file so we can access its code within this file
 require_relative './vehicle'
+
+# Car is a baby of Vehicle
+# Vehicle is the "superclass" of Car
+class Car < Vehicle
+    
+    # overwrites Vehicle's go method (b/c same name & baby takes precident)
+    def go
+        "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+    end
+end
